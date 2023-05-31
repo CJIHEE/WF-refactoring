ca99b78764134b2bb271cb8d7db9784a1a935c2a
Jihee Choi <chlwlgml121@naver.com>
2023-05-31 18:34:02 +0900

    local,dev,test 환경별 DB 분리

--- employee-api/src/test/java/com/workFlow/WFrefactoring/TestEmployeeService.java
+++ employee-api/src/test/java/com/workFlow/WFrefactoring/TestEmployeeService.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
@@ -23,6 +24,7 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
         DirtiesContextTestExecutionListener.class
 })
 @SpringBootTest
+@ActiveProfiles("test")
 public class TestEmployeeService {
     @Autowired
     private EmployeeService employeeService;
