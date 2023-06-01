db315bc4a15815bba83f0dde9e276bfe6907db62
Jihee Choi <chlwlgml121@naver.com>
2023-06-01 17:38:59 +0900

    deptDto builder 사용

--- domain/src/main/java/com/workFlow/WFrefactoring/dept/dto/DeptDto.java
+++ domain/src/main/java/com/workFlow/WFrefactoring/dept/dto/DeptDto.java
@@ -1,22 +1,25 @@
 package com.workFlow.WFrefactoring.dept.dto;
 
 import com.workFlow.WFrefactoring.model.Dept;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class DeptDto {
     private Integer deptNo;
     private String deptName;
     private Long leadEmpNo;
     private Integer upperDeptNo;
 
-    public static DeptDto deptEntityToDeptDto(Dept dept) {
-        DeptDto deptDto = new DeptDto();
-        deptDto.deptNo = dept.getDeptNo();
-        deptDto.deptName = dept.getDeptName();
-        deptDto.leadEmpNo = dept.getLeadEmpNo();
-        deptDto.upperDeptNo = dept.getUpperDeptNo();
-        return deptDto;
+    public static DeptDto deptEntityToDeptDto(Dept dept){
+        return DeptDto.builder()
+                .deptNo(dept.getDeptNo())
+                .deptName(dept.getDeptName())
+                .leadEmpNo(dept.getLeadEmpNo())
+                .upperDeptNo(dept.getUpperDeptNo())
+                .build();
     }
+
 
 }
