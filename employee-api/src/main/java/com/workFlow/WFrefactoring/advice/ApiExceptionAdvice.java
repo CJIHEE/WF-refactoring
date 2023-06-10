9bba99f404fdce294e607dc94eeeb88ffe324d0e
Jihee Choi <chlwlgml121@naver.com>
2023-06-10 15:38:17 +0900

    dev 환경에서만 advice 적용

--- employee-api/src/main/java/com/workFlow/WFrefactoring/advice/ApiExceptionAdvice.java
+++ employee-api/src/main/java/com/workFlow/WFrefactoring/advice/ApiExceptionAdvice.java
@@ -1,11 +1,13 @@
 package com.workFlow.WFrefactoring.advice;
 
 import com.workFlow.WFrefactoring.exception.ApiException;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Profile("dev")
 public class ApiExceptionAdvice {
 
     @ExceptionHandler(ApiException.class)
