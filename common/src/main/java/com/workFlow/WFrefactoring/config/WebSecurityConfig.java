8cd6906edd113097c5cb16513ca8b18590214a86
Jihee Choi <chlwlgml121@naver.com>
2023-06-13 15:26:33 +0900

    SecurityFilterChain으로 변경

--- common/src/main/java/com/workFlow/WFrefactoring/config/WebSecurityConfig.java
+++ common/src/main/java/com/workFlow/WFrefactoring/config/WebSecurityConfig.java
@@ -5,25 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChian(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/employees").permitAll()
                 .anyRequest().authenticated();
         http.csrf().disable();
 
+        return http.build();
     }
 }
